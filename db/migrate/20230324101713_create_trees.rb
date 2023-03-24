3c3f33b22530217ae83f57c70c6b12aa3e479c92
Adrien Lupo <adrien.lupo@protonmail.com>
2023-03-24 12:46:43 +0100

    draft models

---
+++ db/migrate/20230324101713_create_trees.rb
@@ -0,0 +1,14 @@
+class CreateTrees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trees do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+      t.string :fruit_type
+      t.integer :price_per_year
+      t.integer :quantity_per_year
+
+      t.timestamps
+    end
+  end
+end
